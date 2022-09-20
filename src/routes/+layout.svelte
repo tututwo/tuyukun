7a824a66e7becfeef912561ea9b34cede85194d0
tututwo <tugordon@outlook.com>
2022-09-19 21:41:45 -0400

    nav sections done: font and icon design needed

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,89 @@
+<script>
+  import "../app.css";
+  import Icon from "@iconify/svelte";
+
+  import { Annotation, annotate } from "svelte-rough-notation";
+  import { annotationGroup } from "rough-notation";
+  import { onMount } from "svelte";
+
+ 
+  let navOptions = [
+    { name: "Blog", visible: false },
+    { name: "Project", visible: false },
+    { name: "About", visible: false },
+  ];
+</script>
+
+<nav class="bg-white shadow drop-shadow-lg flex justify-between">
+  <!--! Left part  -->
+  <div class="container flex h-16">
+    <div class="flex items-center px-2">
+      <a
+        class="lg:text-3xl font-bold text-gray-900 hover:text-gray-400"
+        href="/">Gordon Tu</a
+      >
+    </div>
+
+    <div class="lg:flex items-center">
+      <!--* Nav Options  -->
+      {#each navOptions as {name, visible}}
+          <!-- svelte-ignore a11y-mouse-events-have-key-events -->
+          <a
+          on:mouseover={(d) => (visible = true)}
+          on:mouseout={(d) => (visible = false)}
+          on:focus={(d) => (visible = true)}
+          use:annotate={{
+            visible: visible,
+            color: "#8fb2c9",
+            type: "underline",
+            animationDuration: 1000,
+          }}
+          href="/{name.toLowerCase()}"
+          class="m-2 font-medium text-lg text-gray-900">{name}</a
+        >
+      {/each}
+    </div>
+  </div>
+  <!--! Right part  -->
+  <div class="flex items-center justify-center px-2 lg:justify-end">
+    <!--* Search Bar  -->
+
+    <div class="relative">
+      <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+        <svg
+          class="w-4 h-4 text-gray-600 dark:text-gray-300"
+          viewBox="0 0 24 24"
+          fill="none"
+        >
+          <path
+            d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </span>
+
+      <input
+        type="text"
+        class="block w-full py-1 pl-10 pr-4 text-gray-700 placeholder-gray-600 bg-white border-b border-gray-600 dark:placeholder-gray-300 dark:focus:border-gray-300 lg:w-56 lg:border-transparent dark:bg-gray-800 dark:text-gray-300 focus:outline-none focus:border-gray-600"
+        placeholder="Search"
+      />
+    </div>
+
+    <!--* Social Media Icons  -->
+    <div class="flex mr-10">
+      <a href="https://twitter.com/tu_yukun" class="px-2"
+        ><Icon class="hover:animate-spin" icon="akar-icons:twitter-fill" /></a
+      >
+      <a href="https://github.com/tututwo" class="px-2"
+        ><Icon icon="akar-icons:github-fill" /></a
+      >
+      <a href="https://observablehq.com/@tututwo?tab=profile" class="px-2"
+        ><Icon icon="simple-icons:observable" /></a
+      >
+    </div>
+  </div>
+</nav>
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,112 @@
+<script>
+  let height = 10;
+</script>
+
+<header>This the creative section</header>
+
+<main>
+  Project Card Comes Here
+
+  <!--* Project  -->
+  <section class="container grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+    <article class="col-span-1 p-4">
+      <a
+        href="https://blog.datawrapper.de/wp-content/uploads/2022/08/image3-3.png"
+        class="relative"
+      >
+        <img
+          alt="foreground"
+          class="absolute inset-0 w-full object-cover object-center"
+          src="https://blog.datawrapper.de/wp-content/uploads/2022/08/image3-3.png"
+        />
+        <div
+          class="py-10 w-full h-full bg-gray-300 opacity-0 hover:opacity-70 transition duration-1000 ease-in-out"
+          >
+          <h2>The elevation of Jiangxi Province</h2>
+          <p class="text-black">
+            This is a water-ink styled map of my hometown, Jiangxi Province, in
+            China. The map is created with QGIS.
+          </p>
+        </div>
+      </a>
+    </article>
+
+    <article class="col-span-1 p-4">
+      <a href="https://tuyukun.com/project/map_shuimo/" class="relative">
+        <!--? How to make sure the article grow along with the img instead of the py-n  -->
+        <picture>
+          <img
+            alt="foreground"
+            class="absolute inset-0 w-full h-full object-cover object-center"
+            src="https://blog.datawrapper.de/wp-content/uploads/2022/08/image11-3.png"
+          />
+        </picture>
+        <div
+          class="py-[400px] w-full h-full bg-cyan-500 opacity-0 hover:opacity-80 transition duration-1000 ease-in-out"
+        >
+          <h2 class="">The elevation of Jiangxi Province</h2>
+          <p class="text-white">
+            This is a water-ink styled map of my hometown, Jiangxi Province, in
+            China. The map is created with QGIS.
+          </p>
+        </div>
+      </a>
+    </article>
+    <article class="col-span-1 p-4">
+      <a href="https://tuyukun.com/project/map_shuimo/" class="relative">
+        <img
+          alt="foreground"
+          class="absolute inset-0 w-full h-full object-cover object-center"
+          src="https://blog.datawrapper.de/wp-content/uploads/2022/08/image1-3.png"
+        />
+        <div
+          class="w-full h-full bg-cyan-500 opacity-0 hover:opacity-80 transition duration-1000 ease-in-out"
+        >
+          <h2>The elevation of Jiangxi Province</h2>
+          <p class="text-white">
+            This is a water-ink styled map of my hometown, Jiangxi Province, in
+            China. The map is created with QGIS.
+          </p>
+        </div>
+      </a>
+    </article>
+    <article class="col-span-1 p-4">
+      <a href="https://tuyukun.com/project/map_shuimo/" class="relative">
+        <img
+          alt="foreground"
+          class="absolute inset-0 w-full h-full object-cover object-center"
+          src="https://blog.datawrapper.de/wp-content/uploads/2022/08/image13-4.png"
+        />
+        <div
+          class="w-full h-full bg-cyan-500 opacity-0 hover:opacity-80 transition duration-1000 ease-in-out"
+        >
+          <h2>The elevation of Jiangxi Province</h2>
+          <p class="text-white">
+            This is a water-ink styled map of my hometown, Jiangxi Province, in
+            China. The map is created with QGIS.
+          </p>
+        </div>
+      </a>
+    </article>
+    <article class="col-span-1 p-4">
+      <a href="https://tuyukun.com/project/map_shuimo/" class="relative">
+        <img
+          alt="foreground"
+          class="absolute inset-0 w-full object-cover object-center"
+          src="https://blog.datawrapper.de/wp-content/uploads/2022/08/image10-3.png"
+        />
+        <div
+          class="w-full h-full bg-cyan-500 opacity-0 hover:opacity-80 transition duration-1000 ease-in-out"
+        >
+          <h2>The elevation of Jiangxi Province</h2>
+          <p class="text-white">
+            This is a water-ink styled map of my hometown, Jiangxi Province, in
+            China. The map is created with QGIS.
+          </p>
+        </div>
+      </a>
+    </article>
+  </section>
+</main>
+
+<footer>Give thanks to family and friends</footer>
